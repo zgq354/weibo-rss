73f7bb82b7e30dd5c0f9663df31c8c3847dc3264
qing <1017967407@qq.com>
2017-10-01 14:30:01 +0800

    [ADD] 实现基本功能

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var weibo = require('./routes/weibo');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('common'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/rss', weibo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/weibo.js
@@ -0,0 +1,113 @@
+/**
+ * Created by qing on 17-9-30.
+ */
+var express = require('express');
+var axios = require('axios');
+var RSS = require('rss');
+var router = express.Router();
+
+const API_URL = 'https://m.weibo.cn/api/container/getIndex';
+const DETAIL_URL = 'https://m.weibo.cn/status/';
+const PROFILE_URL = 'https://weibo.com/';
+
+/* GET weibo rss. */
+router.get('/:id', function(req, res, next) {
+  // 传入微博用户的uid
+  var id = req.params['id'];
+
+  // 验证参数格式
+  if (!/^[0-9]*$/.test(id)) {
+    console.log("Invalid Format");
+    return next();
+  }
+
+  var feed; // feed 对象
+
+  // 第一步，获取用户的信息
+  axios.get(API_URL, {
+    params: {
+      type: 'uid',
+      value: id
+    }
+  }).then(function (res) {
+    // 初始化 feed对象
+    feed = new RSS({
+      site_url: PROFILE_URL + res.data.userInfo.id,
+      title: res.data.userInfo.screen_name + '的微博',
+      description: res.data.userInfo.description,
+      generator: 'weibo-rss',
+      ttl: 10
+    });
+
+    // 获取container id
+    const containerId = res.data.tabsInfo.tabs[1].containerid;
+    // 下一步，获取用户最近的微博
+    return axios.get(API_URL, {params: { type: 'uid', value: id, containerid: containerId }});
+  }).then(function (res) {
+    const cards = res.data.cards;
+
+    // 过滤掉多余的card
+    var list = cards.filter(function (item) {
+      return item.card_type == 9;
+    });
+
+    // 分别抓取每一条微博的具体内容，只有访问每条微博的详情的时候才能获取到完整的全文和时间信息
+    var listPromises = [];
+    list.forEach(function (item) {
+      listPromises.push(axios.get(DETAIL_URL + item.mblog.id));
+    });
+
+    // 下一步：处理构造好的Promise的并发请求结果
+    return axios.all(listPromises);
+  }).then(function (resArr) {
+    resArr.forEach(function (res) {
+      var data = res.data;
+
+      // 先去掉换行，再提取json
+      data = data.replace(/\n/g, '').match(/\$render_data\s\=\s\[(.*?\})\]\[0\]/);
+      data = data ? data[1] : {};
+      data = JSON.parse(data);
+
+      // 构造feed中的item
+      feed.item({
+        title: data.status.status_title,
+        description: format_status(data.status),
+        url: DETAIL_URL + data.status.id,
+        guid: DETAIL_URL + data.status.id,
+        date: new Date(data.status.created_at)
+      });
+    });
+
+    // 发送结果
+    res.header('Content-Type', 'text/xml');
+    res.send(feed.xml());
+  });
+});
+
+// 格式化每条微博的HTML
+function format_status(status) {
+  // 长文章的处理
+  var temp = status.longText ? status.longText.longTextContent.replace(/\n/g, '<br>')
+      : status.text.replace(/\<span\sclass\=\"url\-icon\"\>.*?\<\/span\>/g, '');
+  temp += "<br><br>";
+
+  // 处理转发的微博
+  if (status.retweeted_status) {
+    // console.log(status.retweeted_status);
+    temp += '转发 <a href="' + PROFILE_URL + status.retweeted_status.user.id + '" target="_blank">@' +
+        status.retweeted_status.user.screen_name +
+        '</a>: ';
+    // 插入转发的微博
+    temp += format_status(status.retweeted_status);
+  }
+
+  // 添加微博配图
+  if (status.pics) {
+    status.pics.forEach(function (item) {
+      temp += '<img src="' + item.url + '"><br><br>';
+    });
+  }
+  return temp;
+}
+
+module.exports = router;

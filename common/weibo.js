834aa4e82da74ff504b789b0e81900645c39cdf0
qing <zgq354@foxmail.com>
2018-05-14 09:41:32 +0800

    [Fix] remove url-icon #10

--- common/weibo.js
+++ common/weibo.js
@@ -258,7 +258,7 @@ function formatStatus(status) {
   // 表情图标转换为文字
   temp = temp.replace(/<span class="url-icon"><img src=".*?" style="width:1em;height:1em;" alt="(.*?)"><\/span>/g, '$1');
   // 去掉外部链接的图标
-  temp = temp.replace(/<span class="url-icon"><img src=".*?"><\/span><\/i>/g, '');
+  temp = temp.replace(/<span class="url-icon"><img src=".*?"><\/span>/g, '');
   // 去掉多余无意义的标签
   temp = temp.replace(/<span class="surl-text">/g, '');
   // 最后插入两个空行，让转发的微博排版更加美观一些

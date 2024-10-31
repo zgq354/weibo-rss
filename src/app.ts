5668d89cd313be66239916b3962dc5c51485c477
bugyaluwang <jiabowang@moego.pet>
2024-10-31 18:16:23 +0800

    chore: add node-network-devtools to visualize network requests in the development environment on Chrome Devtools

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-import Koa from 'koa';
-import Router from '@koa/router';
-import serve from 'koa-static';
-import path from 'path';
-import config from './config';
-import { logger } from "./modules/logger";
-import { normalizePort } from './utils';
-import { registerRoutes } from './modules/routes';
-import { RSSKoaContext, RSSKoaState } from './types';
-import { LevelCache } from './modules/cache';
-import { WeiboData } from './modules/weibo/weibo';
+import Koa from "koa";
+import Router from "@koa/router";
+import serve from "koa-static";
+import path from "path";
+import config from "./config";
+import { debug, logger } from "./modules/logger";
+import { normalizePort } from "./utils";
+import { registerRoutes } from "./modules/routes";
+import { RSSKoaContext, RSSKoaState } from "./types";
+import { LevelCache } from "./modules/cache";
+import { WeiboData } from "./modules/weibo/weibo";
+import { register } from "node-network-devtools";
+
+debug && register();
 
 const koaApp = new Koa<RSSKoaState, RSSKoaContext>();
 const initApp = () => {
@@ -16,7 +19,10 @@ const initApp = () => {
   registerRoutes(router);
 
   // levelCache
-  const cache = LevelCache.getInstance(path.join(config.rootDir, 'data'), logger);
+  const cache = LevelCache.getInstance(
+    path.join(config.rootDir, "data"),
+    logger
+  );
   cache.startScheduleCleanJob();
 
   // weibo
@@ -33,14 +39,18 @@ const initApp = () => {
       logger.debug(`${ctx.req.method} ${ctx.originalUrl} ${ctx.ip}`);
       await next();
       const duration = Date.now() - startTime;
-      logger.info(`[${ctx.status}] ${ctx.req.method} ${ctx.originalUrl} ${ctx.ip} hit: ${ctx.state.hit || 0} ${duration}ms`);
+      logger.info(
+        `[${ctx.status}] ${ctx.req.method} ${ctx.originalUrl} ${ctx.ip} hit: ${
+          ctx.state.hit || 0
+        } ${duration}ms`
+      );
     })
     .use(async (ctx, next) => {
       ctx.cache = cache;
       ctx.weibo = weiboData;
       await next();
     })
-    .use(serve(config.rootDir + '/public'))
+    .use(serve(config.rootDir + "/public"))
     .use(router.routes());
 };
 
